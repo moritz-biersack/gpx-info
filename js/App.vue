2eceb00a23c6374ab7fe398108f493459290a4f6
Moritz <moritz@rfrnz.com>
2020-11-23 20:00:05 +0100

    Init commit

---
+++ js/App.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .container.bg-red-100 Hello {{bundler}}
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+});
+</script>
